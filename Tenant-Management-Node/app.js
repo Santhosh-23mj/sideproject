40692368f01cca9294677ad127df652e8471fc5a
n00bie <santhosh23mj@gmail.com>
2021-04-17 18:03:21 +0530

    add basic configurations

--- Tenant-Management-Node/app.js
+++ Tenant-Management-Node/app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// adding custom routes
+const houses = require('./routes/houses');
+const tenants = require('./routes/tenants');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,8 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tenants', tenants);
+app.use('/houses', houses);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
